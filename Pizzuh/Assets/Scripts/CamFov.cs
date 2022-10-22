3c377154db3f8d87c41f480b171d13f326eabebe
Jonathan <56273460+Jonathan-Jay@users.noreply.github.com>
2022-10-22 17:34:22 -0400

    maps + first person mode

--- Pizzuh/Assets/Scripts/CamFov.cs
+++ Pizzuh/Assets/Scripts/CamFov.cs
@@ -1,16 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Cinemachine;
 
 public class CamFov : MonoBehaviour
 {
+	public InputAction firstPerson;
 	[SerializeField]	Rigidbody rb;
-	[SerializeField]	Cinemachine.CinemachineVirtualCamera cam;
+	[SerializeField]	CinemachineVirtualCamera cam;
+	Cinemachine3rdPersonFollow follow;
 	[SerializeField]	float maxSpeed = 50f;
 	[SerializeField]	float minFov = 60f;
 	[SerializeField]	float maxFov = 130f;
 	[SerializeField]	float catchupSpeed = 50f;
+	[SerializeField]	float followDistance = 5f;
+
+	private void Awake() {
+		follow = cam.GetCinemachineComponent<Cinemachine3rdPersonFollow>();
+		firstPerson.started += ctx => follow.CameraDistance = 0f;
+		firstPerson.canceled += ctx => follow.CameraDistance = followDistance;
+	}
+
+	private void OnEnable() {
+		firstPerson.Enable();
+	}
+
+	private void OnDisable() {
+		firstPerson.Disable();
+	}
 
 	private void Start() {
 		transform.SetParent(null);
