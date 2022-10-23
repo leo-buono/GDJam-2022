34739ce63be3c8e290b6a3896a687890991ecccd
Jonathan <56273460+Jonathan-Jay@users.noreply.github.com>
2022-10-23 00:39:56 -0400

    fixed some stuff

--- Pizzuh/Assets/Scripts/MeshCloner.cs
+++ Pizzuh/Assets/Scripts/MeshCloner.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class MeshCloner : MonoBehaviour
 {
-	private void Update() {
-		if (LoopPosition.instance.target.position.x > 0f &&
-			transform.position.x < LoopPosition.instance.transposAmt * -0.25f) {
-				transform.position += Vector3.right * LoopPosition.instance.transposAmt;
+	private void LateUpdate() {
+		if (LoopPosition.instance.target.position.x - transform.position.x < LoopPosition.instance.transposAmt * -0.4f) {
+				transform.position += Vector3.left * LoopPosition.instance.transposAmt;
 		}
-		if (LoopPosition.instance.target.position.x < 0f &&
-			transform.position.x > LoopPosition.instance.transposAmt * 0.25f) {
-			transform.position += Vector3.left * LoopPosition.instance.transposAmt;
+		if (LoopPosition.instance.target.position.x - transform.position.x > LoopPosition.instance.transposAmt * 0.4f) {
+			transform.position += Vector3.right * LoopPosition.instance.transposAmt;
 		}
 	}
 }
