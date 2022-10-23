30ff3f1f709f59bdbfe150b3b244f04bda8a514f
GlobalBooga <alex_anasta@hotmail.ca>
2022-10-23 16:47:20 -0400

    no

--- Pizzuh/Assets/Scripts/Score.cs
+++ Pizzuh/Assets/Scripts/Score.cs
@@ -2,15 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Score : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private InputAction g;
+    private TextMeshProUGUI newText;
     private int score = 0;
+    private bool addingScore = false;
+
+    private void Awake()
+    {
+        g.started += ctx =>
+        {
+            AddScore(100);
+        };
+    }
+
+    private void FixedUpdate()
+    {
+        if (addingScore)
+        {
+            newText.GetComponent<RectTransform>().position += new Vector3();
+        }
+    }
 
     public void AddScore(int amount)
     {
         score += amount;
         text.text = score.ToString();
+
+        newText = Instantiate(text, gameObject.transform);
+        newText.color = Color.clear;
+        addingScore = true;
     }
 }
