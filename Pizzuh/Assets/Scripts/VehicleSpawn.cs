fa235ee65ff79b3907a519f293c89a680ad60608
leo-buono <56273373+leo-buono@users.noreply.github.com>
2022-10-22 22:53:16 -0400

    random car generation
    cars 3 is real

--- Pizzuh/Assets/Scripts/VehicleSpawn.cs
+++ Pizzuh/Assets/Scripts/VehicleSpawn.cs
@@ -8,6 +8,10 @@ public class VehicleSpawn : MonoBehaviour
     public GameObject player;
     public Vector2 lowerRange;
     public Vector2 upperRange;
+    public int carCount = 10;
+
+    Queue<GameObject> cars = new Queue<GameObject>();
+    Queue<GameObject> cars2 = new Queue<GameObject>();
 
     private bool isInLowerRange = false;
     // Update is called once per frame
@@ -16,15 +20,27 @@ public class VehicleSpawn : MonoBehaviour
         if(player.transform.position.x > lowerRange.y && isInLowerRange)
         {
             isInLowerRange = false;
+            SpawnCar(isInLowerRange, cars);
         }
         else if (player.transform.position.x > lowerRange.x && player.transform.position.x < lowerRange.y && !isInLowerRange)
         {
             isInLowerRange = true;
+            SpawnCar(isInLowerRange, cars2);
         }
     }
 
-    void SpawnCar(bool rangeType)
+    void SpawnCar(bool rangeType, Queue<GameObject> cars)
     {
-        //Instatiate();
+        if(cars.Count >= carCount)
+        {
+            Destroy(cars.Dequeue());
+        }
+        int random = Random.Range(0, carList.Count);
+        float randomPos = 0f;
+        if(isInLowerRange)
+            randomPos = Random.Range(lowerRange.x + 10, lowerRange.y - 10);
+        else 
+            randomPos = Random.Range(upperRange.x + 10, upperRange.y - 10);
+        cars.Enqueue(Instantiate(carList[random], new Vector3(randomPos, 2.5f, Random.Range(-0.5f, 0.5f)), Quaternion.identity));
     }
 }
