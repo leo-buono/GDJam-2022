b99c3edbcf9644a78dbec3c8dcba4e23f2a8b439
leo-buono <56273373+leo-buono@users.noreply.github.com>
2022-10-23 01:02:54 -0400

    Added car despawn so that there is more diversity and less staying behind

--- Pizzuh/Assets/Scripts/VehicleSpawn.cs
+++ Pizzuh/Assets/Scripts/VehicleSpawn.cs
@@ -8,7 +8,8 @@ public class VehicleSpawn : MonoBehaviour
     public GameObject player;
     public Vector2 lowerRange;
     public Vector2 upperRange;
-    public int carCount = 10;
+    public int carLimit = 10;
+    public int carSpawn = 3;
 
     Queue<GameObject> cars = new Queue<GameObject>();
     Queue<GameObject> cars2 = new Queue<GameObject>();
@@ -19,28 +20,33 @@ public class VehicleSpawn : MonoBehaviour
     {
         if(player.transform.position.x > lowerRange.y && isInLowerRange)
         {
-            isInLowerRange = false;
             SpawnCar(isInLowerRange, cars);
+            isInLowerRange = false;
         }
         else if (player.transform.position.x > lowerRange.x && player.transform.position.x < lowerRange.y && !isInLowerRange)
         {
-            isInLowerRange = true;
             SpawnCar(isInLowerRange, cars2);
+            isInLowerRange = true;
         }
     }
 
     void SpawnCar(bool rangeType, Queue<GameObject> cars)
     {
-        if(cars.Count >= carCount)
+        for (int i = 0; cars.Count > 0; i++)
         {
             Destroy(cars.Dequeue());
+            Debug.Log("Deleted Vehicle");
         }
-        int random = Random.Range(0, carList.Count);
-        float randomPos = 0f;
-        if(isInLowerRange)
-            randomPos = Random.Range(lowerRange.x + 10, lowerRange.y - 10);
-        else 
-            randomPos = Random.Range(upperRange.x + 10, upperRange.y - 10);
+        for (int i = 0; i < carSpawn; i++)
+        {
+            int random = Random.Range(0, carList.Count);
+            float randomPos = 0f;
+            if(isInLowerRange)
+                randomPos = Random.Range(lowerRange.x + 10, lowerRange.y - 10);
+            else {
+                randomPos = Random.Range(upperRange.x + 10, upperRange.y - 10);
+            }
             cars.Enqueue(Instantiate(carList[random], new Vector3(randomPos, 2.5f, Random.Range(-3f, 3f)), Quaternion.identity));
+        }
     }
 }
