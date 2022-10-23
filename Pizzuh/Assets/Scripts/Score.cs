c5adbbdf922e1d9d32a0a7f87877a4b27a2330ff
Jonathan <56273460+Jonathan-Jay@users.noreply.github.com>
2022-10-23 17:54:18 -0400

    ev evrything

--- Pizzuh/Assets/Scripts/Score.cs
+++ Pizzuh/Assets/Scripts/Score.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
@@ -9,10 +10,13 @@ public class Score : MonoBehaviour
     static public int score = 0;
     int displayScore = 0;
 	int multiplier = 3;
+	public Image fire;
+
     void Start()
     {
         text = GetComponentInChildren<TextMeshProUGUI>();
 		text.text = "Score : 0";
+		fire.rectTransform.localScale = Vector3.zero;
     }
     static public void AddScore(int amount)
     {
@@ -25,6 +29,14 @@ public class Score : MonoBehaviour
 			if (displayScore > score)
 				displayScore = score;
         	text.text = "Score : " + displayScore.ToString();
+			fire.rectTransform.localScale = Vector3.MoveTowards(fire.rectTransform.localScale, Vector3.one, Time.deltaTime * 5f);
 		}
+		else {
+			fire.rectTransform.localScale = Vector3.MoveTowards(fire.rectTransform.localScale, Vector3.zero, Time.deltaTime * 2f);
+		}
+	}
+
+	private void OnDestroy() {
+		score = 0;
 	}
 }
