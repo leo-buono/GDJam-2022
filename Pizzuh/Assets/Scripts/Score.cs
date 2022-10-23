7719357a8b65d3ee10761ff5235008e0b0909c43
Jonathan <56273460+Jonathan-Jay@users.noreply.github.com>
2022-10-23 17:23:38 -0400

    this

--- Pizzuh/Assets/Scripts/Score.cs
+++ Pizzuh/Assets/Scripts/Score.cs
@@ -7,13 +7,24 @@ public class Score : MonoBehaviour
 {
     static public TextMeshProUGUI text;
     static public int score = 0;
+    int displayScore = 0;
+	int multiplier = 3;
     void Start()
     {
-        text = GetComponentInChildren<TextMeshProUGUI>();      
+        text = GetComponentInChildren<TextMeshProUGUI>();
+		text.text = "Score : 0";
     }
     static public void AddScore(int amount)
     {
         score += amount;
-        text.text = "Score : " + score.ToString();
     }
+
+	private void Update() {
+		if (score != displayScore) {
+			displayScore += multiplier;
+			if (displayScore > score)
+				displayScore = score;
+        	text.text = "Score : " + displayScore.ToString();
+		}
+	}
 }
